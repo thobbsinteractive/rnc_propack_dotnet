f77d2af1949b0b2486114dc0f92f22e97b2f2518
Timothy Hobbs <thobbsinteractive@gmail.com>
2024-03-10 21:39:08 +0100

    feat: added more byte reading/writing methods

--- RncProPack.cs
+++ RncProPack.cs
@@ -1,28 +1,77 @@
-﻿namespace rnc_propack_dotnet
+﻿using System;
+
+namespace rnc_propack_dotnet
 {
     public class RncProPack
     {
-        public byte PeekByte(byte[] buf, ulong offset)
+        public byte PeekByte(byte[] buf, int offset)
         {
             return buf[offset];
         }
 
-        public byte ReadByte(byte[] buf, ref ulong offset)
+        public byte ReadByte(byte[] buf, ref int offset)
         {
             return buf[offset++];
         }
 
-        public void WriteByte(byte[] buf, ref ulong offset, byte b)
+        public void WriteByte(byte[] buf, ref int offset, byte b)
         {
             buf[offset++] = b;
         }
 
-        public ushort PeekWordBigEndian(byte[] buf, ulong offset)
+        public ushort PeekWordBigEndian(byte[] buf, int offset)
         {
             byte b1 = PeekByte(buf, offset + 0);
             byte b2 = PeekByte(buf, offset + 1);
 
             return (ushort)((b1 << 8) | b2);
+        }
+        public ushort ReadWordBigEndian(byte[] buf, ref int offset)
+        {
+            byte b1 = ReadByte(buf, ref offset);
+            byte b2 = ReadByte(buf, ref offset);
+
+            return (ushort)((b1 << 8) | b2);
+        }
+
+        public void WriteWordBigEndian(byte[] buf, ref int offset, ushort val)
+        {
+            WriteByte(buf, ref offset, (byte)((val >> 8) & 0xFF));
+            WriteByte(buf, ref offset, (byte)((val >> 0) & 0xFF));
+        }
+
+        public uint PeekDWordBigEndian(byte[] buf, int offset)
+        {
+            ushort w1 = PeekWordBigEndian(buf, offset + 0);
+            ushort w2 = PeekWordBigEndian(buf, offset + 2);
+
+            return ((uint)w1 << 16) | w2;
+        }
+
+        public uint ReadDWordBigEndian(byte[] buf, ref int offset)
+        {
+            ushort w1 = ReadWordBigEndian(buf, ref offset);
+            ushort w2 = ReadWordBigEndian(buf, ref offset);
+
+            return ((uint)w1 << 16) | w2;
+        }
+
+        public void WriteDWordBigEndian(byte[] buf, ref int offset, uint val)
+        {
+            WriteWordBigEndian(buf, ref offset, (ushort)(val >> 16));
+            WriteWordBigEndian(buf, ref offset, (ushort)(val & 0xFFFF));
+        }
+
+        public static void ReadBuffer(byte[] dest, byte[] source, ref int offset, int size)
+        {
+            Buffer.BlockCopy(source, offset, dest, 0, size);
+            offset += size;
+        }
+
+        public static void WriteBuffer(byte[] dest, ref int offset, byte[] source, int size)
+        {
+            Buffer.BlockCopy(source, 0, dest, offset, size);
+            offset += size;
         }
     }
 }
