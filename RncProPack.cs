500630fa008bb1aeb73042bdd4a0c27ba3ba7c1e
Timothy Hobbs <thobbsinteractive@gmail.com>
2024-03-10 21:33:22 +0100

    feat: first implementation of basic read/write methods

---
+++ RncProPack.cs
@@ -0,0 +1,28 @@
+﻿namespace rnc_propack_dotnet
+{
+    public class RncProPack
+    {
+        public byte PeekByte(byte[] buf, ulong offset)
+        {
+            return buf[offset];
+        }
+
+        public byte ReadByte(byte[] buf, ref ulong offset)
+        {
+            return buf[offset++];
+        }
+
+        public void WriteByte(byte[] buf, ref ulong offset, byte b)
+        {
+            buf[offset++] = b;
+        }
+
+        public ushort PeekWordBigEndian(byte[] buf, ulong offset)
+        {
+            byte b1 = PeekByte(buf, offset + 0);
+            byte b2 = PeekByte(buf, offset + 1);
+
+            return (ushort)((b1 << 8) | b2);
+        }
+    }
+}
