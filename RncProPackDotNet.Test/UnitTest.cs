599c7dccd36c3a004b91e6a35323c4f49679e3b8
Timothy Hobbs <thobbsinteractive@gmail.com>
2025-08-26 22:44:27 +0200

    test: hopefully fix tests. change Action build to release

--- RncProPackDotNet.Test/UnitTest.cs
+++ RncProPackDotNet.Test/UnitTest.cs
@@ -25,7 +25,7 @@ public class UnitTest
 
         rncProPack.DoUnpack(ref vars);
 
-        using (FileStream outFile = new FileStream(@"Resources\temp_uncompressed.bin", FileMode.Create, FileAccess.Write))
+        using (FileStream outFile = new FileStream(@"Resources/temp_uncompressed.bin", FileMode.Create, FileAccess.Write))
         {
             outFile.Write(vars.Output, 0, vars.OutputOffset);
         }
@@ -61,7 +61,7 @@ public class UnitTest
             outFile.Write(vars.Output, 0, vars.OutputOffset);
         }
 
-        var outputBytes = File.ReadAllBytes(@"Resources\temp_compressed.bin");
+        var outputBytes = File.ReadAllBytes(@"Resources/temp_compressed.bin");
 
         Assert.That(expectedBytes.Length == outputBytes.Length);
 
